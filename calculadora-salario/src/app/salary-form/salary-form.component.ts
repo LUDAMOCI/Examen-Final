dce09e5a1e844c9d59be4f0f2605f43797d8d472
FluidityCore <lmostaceroc1@upao.edu.pe>
2024-07-12 16:37:24 -0500

    Manejar la lógica de la sumisión del formulario

--- calculadora-salario/src/app/salary-form/salary-form.component.ts
+++ calculadora-salario/src/app/salary-form/salary-form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SalaryService } from '../services/salary.service';
+import { SalaryCalculationRequest } from '../models/salary-calculation-request.model';
+import { SalaryCalculationResponse } from '../models/salary-calculation-response.model';
 
 @Component({
   selector: 'app-salary-form',
-  standalone: true,
-  imports: [],
   templateUrl: './salary-form.component.html',
-  styleUrl: './salary-form.component.css'
+  styleUrls: ['./salary-form.component.css']
 })
 export class SalaryFormComponent {
+  result: SalaryCalculationResponse | null = null;
 
+  constructor(private salaryService: SalaryService) { }
+
+  onSubmit(form: NgForm): void {
+    const request: SalaryCalculationRequest = form.value;
+    this.result = this.salaryService.calculateSalary(request);
+  }
 }
